e312af36ee5d71c34b49d02de301855354c16971
franco sassi <franco.sassi97@gmail.com>
2023-06-17 02:05:54 -0300

    home

--- proyectoDocappoint/src/components/Header/index.jsx
+++ proyectoDocappoint/src/components/Header/index.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import styles from './header.module.css'
+import './header.css'
+import caduceo from "../../assets/imgs/caduceo.png"
 const Header = () => {
   return (
     <div>
-<nav className={styles.container}>
- <li>Home</li>
- <li>Consulta Rapida</li>
- <li>Especialidades</li>
- <li>Registrarse</li>
- <li>Inicio Sesion</li>
-</nav>
+       <header>
+        <nav >
+          <div className='logo'>  
+            <a href=" "><img src= {caduceo} alt="" />  <h2>Docappoint</h2></a>
 
+          </div>
+           <br />
+          <ul>
+            <li><a href="">Home</a>  </li>
+            <li><a href="">Consulta Rapida</a> </li>
+            <li><a href="">Especialidades</a> </li>
+            <li><a href="">Registrarse</a> </li>
+            <li><a href=""> Inicio Sesion</a> </li>
+          </ul>
+        </nav></header>  
+ 
+  
+
+      <section className="imagenFondo">
+        <h1>Encontra ya tu especialista!</h1>
+       </section>
+      <br />
+      <img src="fondoHeader.jpg" alt="" />s
     </div>
   )
 }
